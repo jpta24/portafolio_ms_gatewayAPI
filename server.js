f1bbe26d41baf35911cb7a7c613c4c81881425bb
Jpta24 <jeanpierretorres@gmail.com>
2023-11-13 20:55:18 +0100

    basic setup gateway Api done

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 const app = require('./app');
 
-const {connectDBs} = require('./db/index');
-
-// ℹ️ Sets the PORT for our app to have access to it. If no env has been set, we hard code it to 5008
 const PORT = process.env.PORT || 5008;
 
-connectDBs().then(() => {
-	app.listen(PORT, () => {
-		console.log(`Server listening on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+		console.log(`GatewayAPI listening on http://localhost:${PORT}`);
 	});
-});
