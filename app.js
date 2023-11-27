e4d85c736ae263afb0e87f325b1b88df674414a6
Jpta24 <jeanpierretorres@gmail.com>
2023-11-27 06:31:22 +0100

    installed express http proxy

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ require("dotenv").config();
 // Handles http requests (express is node js framework)
 // https://www.npmjs.com/package/express
 const express = require("express");
+const proxy = require('express-http-proxy');
 
 const app = express();
 
-// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware !
 require("./config")(app);
 
-// 👇 Start handling routes here
-const indexRoutes = require("./routes/index.routes");
-app.use("/api", indexRoutes);
+// Proxy to microservices
+app.use('/api', proxy(process.env.MS_API || 'http://localhost:5011'));
+app.use('/user', proxy(process.env.MS_USER || 'http://localhost:5012'));
+app.use('/experiences', proxy(process.env.MS_EXPERIENCES || 'http://localhost:5013'));
+app.use('/projects', proxy(process.env.MS_PROJECTS || 'http://localhost:5014'));
+app.use('/skills', proxy(process.env.MS_SKILLS || 'http://localhost:5015'));
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app);
