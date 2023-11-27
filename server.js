5c41f843d4594e90ef3af51df04e5143e649a3dd
Jpta24 <jeanpierretorres@gmail.com>
2023-11-27 14:38:46 +0100

    test done

--- server.js
+++ server.js
@@ -3,5 +3,5 @@ const app = require('./app');
 const PORT = process.env.PORT || 5008;
 
 app.listen(PORT, () => {
-		console.log(`GatewayAPI listening on http://localhost:${PORT}`);
+		console.log(`GatewayAPI Server listening on http://localhost:${PORT}`);
 	});
